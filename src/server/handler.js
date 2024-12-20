5bd5ba2b6a373fd21e5ddd94ac3842b063c9cf3b
Linaaa <146501527+2linaa@users.noreply.github.com>
2024-12-20 10:43:25 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -24,7 +24,7 @@ async function postPredictHandler(request, h) {
  
   const response = h.response({
     status: 'success',
-    message: confidenceScore > 0 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
+    message: confidenceScore > 0 ? 'Model is predicted successfully' : 'Model is predicted successfully but under threshold. Please use the correct picture',
     data
   })
   response.code(201);
