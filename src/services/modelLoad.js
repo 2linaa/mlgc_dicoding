bd8fa2ad0b8aaf73a68cf03251cca19852cd719e
Linaaa <146501527+2linaa@users.noreply.github.com>
2024-12-20 10:23:15 +0700

    Update modelLoad.js

--- src/services/modelLoad.js
+++ src/services/modelLoad.js
@@ -2,7 +2,7 @@ import tf from "@tensorflow/tfjs-node";
  
 async function loadModel() {
     try {
-        return await tf.loadGraphModel(process.env.MODEL_URL);
+        return await tf.loadGraphModel('https://storage.googleapis.com/submissionlina/model-in-prod/model.json');
       } catch (error) {
         console.error('Error loading model:', error);
       }}
